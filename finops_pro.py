5ffc3b84b4b55d6118ae567f0ffba825dc6837f3
aabdelhafez <aabdelhafez@paypal.com>
2023-04-26 16:38:18 -0700

    more refactoring

--- finops_pro.py
+++ finops_pro.py
@@ -1,9 +1,10 @@
 import os
-import time
 import requests
 import shutil
 import json
 import subprocess
+import openai
+import regex
 
 from bs4 import BeautifulSoup
 
@@ -12,105 +13,115 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-import openai
-from whisper import Whisper
-import openai_secret_manager
+from selenium.webdriver.chrome.options import Options
 
 # function to manage file system, we need to create a directory for the content,  and delete it if it already exists
-
-
 def manage_file_system(delete_existing=True):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     content_dir = os.path.join(root_dir, 'content')
+    print('content_dir: ', content_dir)
 
     # delete the directory and all its contents
     if delete_existing and os.path.isdir(content_dir):
         shutil.rmtree(content_dir)
+        print('Deleted existing content directory.')
 
     # create content dir if it does not exist
     if not os.path.isdir(content_dir):
         os.mkdir(content_dir)
         os.chdir(content_dir)
+        print ('Created new content directory.')
     return content_dir
 
 # function to initialize the Chrome driver with the capabilities
-
 
 def initialize_driver():
     # Set up capabilities to enable performance logging
     caps = DesiredCapabilities.CHROME
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
 
-    driver = webdriver.Chrome(desired_capabilities=caps)
-    driver.get("https://learn.finops.org/path/finops-certified-professional")
+    # start chrome in headless mode
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=chrome_options, desired_capabilities=caps)
+    print('Driver initialized.')
+
+    driver.get('https://learn.finops.org/path/finops-certified-professional')
+    print('page loaded.')
     return driver
 
 # function to find Sign In Button and click it
 
-
 def do_login(driver):
     user_name = os.environ.get('user_name')
     password = os.environ.get('password')
-
+    sign_in_button_path = '/html/body/div/div[1]/div[1]/div[2]/div[2]/div/div/div/div[1]/p/strong/a'
     sign_in_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, '/html/body/div/div[1]/div[1]/div[2]/div[2]/div/div/div/div[1]/p/strong/a')))
+        (By.XPATH, sign_in_button_path)))
     sign_in_button.click()
+    print('Starting sign in.')
 
     # Wait for page to load
-    user_name_path = "/html/body/div/div[1]/div[2]/div/div/form/div/div[1]/input"
+    user_name_path = '/html/body/div/div[1]/div[2]/div/div/form/div/div[1]/input'
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, user_name_path)))
 
     # Login
     email_input = driver.find_element(By.XPATH, user_name_path)
     email_input.send_keys(user_name)
-
-    password_input = driver.find_element(
-        By.XPATH, "/html/body/div/div[1]/div[2]/div/div/form/div/div[2]/input")
+    password_input_path = '/html/body/div/div[1]/div[2]/div/div/form/div/div[2]/input'
+    password_input = driver.find_element(By.XPATH, password_input_path)
     password_input.send_keys(password)
 
-    login_button = driver.find_element(
-        By.XPATH, "/html/body/div/div[1]/div[2]/div/div/form/div/div[4]/button")
+    login_button_path = '/html/body/div/div[1]/div[2]/div/div/form/div/div[4]/button'
+    login_button = driver.find_element(By.XPATH, login_button_path)
     login_button.click()
-
+    print('Sign in complete.')
     # Wait for page to load
-    # WebDriverWait(driver, 10).until(EC.title_contains("Logged in"))
-
+    #[TODO] WebDriverWait(driver, 10).until(EC.title_contains('Logged in'))
 
 # function to find the course content
 def find_content(driver):
     # get all sections
-    all_sections = driver.find_element(
-        By.XPATH, "/html/body/div/div[1]/div/div[4]/div[2]").text
-    sections_list = all_sections.replace(
-        '\n', ' ').replace('Complete ', '').split(' min ')
+    all_sections_path = '/html/body/div/div[1]/div/div[4]/div[2]'
+    all_sections = driver.find_element(By.XPATH, all_sections_path).text
+    sections_list = all_sections.replace('\n', ' ').replace('Complete ', '').split(' min ')
+    print(f'Retrived {len(sections_list)} sections')
     return sections_list
-
 
 def browse_sections(driver, sections_list, content_dir):
     # go through each section to find sub-sections
     for section_id in range(len(sections_list)):
-        section = driver.find_element(
-            By.XPATH, f"/html/body/div/div[1]/div/div[4]/div[2]/a[{section_id+1}]")
-        section.click()  # Click on section to reveal content
-        # Wait for page to load
-        time.sleep(2)
-        handle_section(driver, section_id, content_dir)
+        section_path = f'/html/body/div/div[1]/div/div[4]/div[2]/a[{section_id+1}]'
+        section = driver.find_element(By.XPATH, section_path)
 
+        # Click on section to reveal content
+        section.click()  
+        # Wait for page to load 
+        section_name_path = '/html/body/div/div[1]/div[1]/div[2]/div/div/div[1]/h1'
+        section_name = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, section_name_path ))).text
+        
+        print(f'section_id: {section_id} section_name: {section_name}')
+        handle_section(driver, section_id, section_name, content_dir)
 
-def handle_section(driver, section_id, content_dir):
+def handle_section(driver, section_id, section_name, content_dir):
     try:
-        section_dir = f"{section_id + 1}-{driver.find_element(By.XPATH,'/html/body/div/div[1]/div[1]/div[2]/div/div/div[1]/h1').text}"
-        section_dir = section_dir.replace(' ', '_')
+        section_name = section_name.replace(' ', '_')
+        section_dir = f'{section_id + 1}-{section_name}'
         os.mkdir(section_dir)
         os.chdir(section_dir)
+        print('section_dir: ', section_dir)
+
         # find each sub-section
-        sub_sections = driver.find_element(
-            By.XPATH, "/html/body/div/div[1]/div[2]/div/div/section[1]/div/div").text.split('\n')
+        all_sub_sections_path = '/html/body/div/div[1]/div[2]/div/div/section[1]/div/div'
+        sub_sections = driver.find_element(By.XPATH, all_sub_sections_path).text.split('\n')
+
         for sub_section_id in range(len(sub_sections)):
-            sub_section = driver.find_element(
-                By.XPATH, f"/html/body/div/div[1]/div[2]/div/div/section[1]/div/div/a[{sub_section_id + 1}]")
+            sub_section_path = f'/html/body/div/div[1]/div[2]/div/div/section[1]/div/div/a[{sub_section_id + 1}]'
+            sub_section = driver.find_element(By.XPATH, sub_section_path)
+
             # go through each sub-section
             section_path = os.path.join(content_dir, section_dir)
             handle_sub_sections(driver, sub_section, section_path)
@@ -124,50 +135,48 @@ def handle_section(driver, section_id, content_dir):
         driver.back()
         return
 
-
 def handle_sub_sections(driver, sub_section, section_path):
-    sub_section_name = sub_section.text
+    # split sub_sections string into two using regex e.g. 'Video Introduction 0 hr 8 min' to 'Video Introduction' and '0 hr 8 min'        
+    sub_section_name = regex.split('(\d+ hr \d+ min)', sub_section.text)[0].strip()
+    # trim the space at the end of sub_section_name
     sub_section.click()
     content_type = get_content_type(driver, sub_section_name)
+    print('content_type: ', content_type)
+
     if content_type == 'video':
         handle_video_content(driver, section_path, sub_section_name)
     elif content_type == 'pdf':
         handle_pdf_content(driver)
     elif content_type == 'text':
         handle_text_content(driver, sub_section_name)
-    # Save section content to file
-
+    print('content_type: ', content_type)    
     # go back to section
     driver.back()
     # go back to main
     os.chdir(section_path)
-
 
 def get_content_type(driver, sub_section_name):
     page_source = driver.page_source
     # parse the page source using BeautifulSoup
     soup = BeautifulSoup(page_source, 'html.parser')
     # find the 'a' tag with the href attribute containing the URL of the PDF
-    pdf_link = soup.find('a', target="_blank")
+    pdf_link = soup.find('a', target='_blank')
     if pdf_link is not None:
         # get the URL of the PDF
         pdf_url = pdf_link['href']
         if pdf_url.find('.pdf') > -1:
             return 'pdf'
-
     # check if the subsection contains video based on the text
     if sub_section_name.find('Video') > -1:
         return 'video'
     else:
         return 'text'
 
-
 def handle_text_content(driver, sub_section_name):
     # text only, save to file
-    with open(f"{sub_section_name}.txt", "w", encoding="utf-8") as f:
-        f.write(driver.find_element(By.XPATH,
-                                    '/html/body/div/div[1]/div/div[2]/div/div/div[2]/div').text)
-
+    text_path = '/html/body/div/div[1]/div/div[2]/div/div/div[2]/div'
+    with open(f'{sub_section_name}.txt', 'w', encoding='utf-8') as f:
+        f.write(driver.find_element(By.XPATH, text_path).text)
 
 def handle_pdf_content(driver):
     # if pdf found, download
@@ -176,7 +185,7 @@ def handle_pdf_content(driver):
     # parse the page source using BeautifulSoup
     soup = BeautifulSoup(page_source, 'html.parser')
     # find the 'a' tag with the href attribute containing the URL of the PDF
-    pdf_link = soup.find('a', target="_blank")
+    pdf_link = soup.find('a', target='_blank')
     if pdf_link is not None:
         # get the URL of the PDF
         pdf_url = pdf_link['href']
@@ -190,19 +199,18 @@ def handle_pdf_content(driver):
                 f.write(response.content)
                 f.close()
 
-
 def handle_video_content(driver, section_path, sub_section_name):
     # Set the name of the output file
-    mp4_file = f"{sub_section_name}.mp4".replace(' ', '_').replace(':', '')
-
-    # sub_section.click()
-    # time.sleep(2)
+    mp4_file = f'{sub_section_name}.mp4'.replace(' ', '_').replace(':', '')
+    mp4_file = os.path.join(section_path, mp4_file)
 
     # find the mp4 in the page
     logs = driver.get_log('performance')
 
     # Extract the m3u8 link from the logs
     m3u8_url = None
+
+    print('Searching for video link')  
     for log in logs:
         message = json.loads(log['message'])
         if 'Network.responseReceived' in message['message']['method']:
@@ -210,6 +218,7 @@ def handle_video_content(driver, section_path, sub_section_name):
             if 'response' in params:
                 url = params['response']['url']
                 if url.endswith('.m3u8'):
+                    print('Found video link, downloading')
                     m3u8_url = url
                     # first download mp4 from m3u8 using ffmpeg
                     subprocess.run(
@@ -217,26 +226,33 @@ def handle_video_content(driver, section_path, sub_section_name):
                     # then transcribe text from video
                     transcribe_mp4_to_text(mp4_file)
                     # then extract slides from video
-                    extract_slides_from_mp4(os.path.join(section_path, mp4_file))
+                    extract_slides_from_mp4(mp4_file)
                     break
 
 # transcribe mp4 to text using open AI Whisper API
 def transcribe_mp4_to_text(mp4_file):
+    print('transcribing mp4 to text')
  # convert mp4 to mp3
+    print('converting mp4 to mp3')
     mp3_file = mp4_file.replace('\\', '').replace('.mp4', '.mp3')
     subprocess.call(['ffmpeg', '-i', mp4_file.replace('\\', ''),
                     '-vn', '-ar', '44100', '-ac', '2', '-b:a', '192k', mp3_file])
-
+    print('transcribing mp3 to text')
     openai.api_key = os.environ.get('open_ai_api')
-    audio_file = open(mp3_file, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    # save to text, file
-    with open(mp3_file.replace('.mp3', '.txt'), "w", encoding="utf-8") as f:
-        f.write(transcript.text)
-        f.close()
+    audio_file = open(mp3_file, 'rb')
+    try:
+        transcript = openai.Audio.transcribe('whisper-1', audio_file)
+        # save to text, file
+        print('saving transcript to text file')
+        with open(mp3_file.replace('.mp3', '.txt'), 'w', encoding='utf-8') as f:
+            f.write(transcript.text)
+            f.close()
+    except:
+        print('error transcribing mp4 to text')
 
 # extract slides from mp4 using slide-extractor cli tool
 def extract_slides_from_mp4(mp4_file):
+    print('Extracting slides from mp4')
     mp4_folder = os.path.dirname(os.path.abspath(mp4_file))
     subprocess.call(['slide-extractor', '-p', mp4_file, mp4_folder])
 
@@ -258,5 +274,5 @@ def main():
 
     driver.quit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
